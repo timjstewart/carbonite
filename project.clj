15373ca414fdd4c99a9d0aea57e696a5b9cca2da
Sam Ritchie <sritchie09@gmail.com>
2011-12-02 08:38:04 -0800

    Aot javabridge.

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.googlecode/kryo "1.04"]]
   :dev-dependencies [[swank-clojure "1.3.3"]]
-  :aot [carbonite.api]
+  :aot [carbonite.JavaBridge]
   :warn-on-reflection true)
