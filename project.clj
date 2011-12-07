cfc08576cac21ea1adf6d1ff0588c6144765e677
Sam Ritchie <sritchie09@gmail.com>
2011-12-07 10:19:44 -0800

    moved java and clojure into separate subdirectories of src. Static methods in JavaBridge are now public.

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject cascalog/carbonite "1.0.2"
+(defproject cascalog/carbonite "1.0.3"
+  :source-path "src/clj"
+  :java-source-path "src/jvm"
   :description "Write Clojure data to and from bytes using Kryo."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.googlecode/kryo "1.04"]]
   :dev-dependencies [[swank-clojure "1.3.3"]]
-  :aot [carbonite.JavaBridge]
   :warn-on-reflection true)
