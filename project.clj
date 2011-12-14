f9b52516265afca8c65a908168baada0c69c5ff2
Sam Ritchie <sritchie09@gmail.com>
2011-12-13 23:45:22 -0800

    Removed swank-clojure dep.

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :description "Write Clojure data to and from bytes using Kryo."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [com.googlecode/kryo "1.04"]]
-  :dev-dependencies [[swank-clojure "1.3.3"]
-                     [lein-multi "1.1.0-SNAPSHOT"]]
+  :dev-dependencies [[lein-multi "1.1.0-SNAPSHOT"]]
   :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
                       [com.googlecode/kryo "1.04"]]
                "1.4" [[org.clojure/clojure "1.4.0-alpha2"]
