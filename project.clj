d7629f28ca4d8e1321d0637f5b721a556cbb540f
Nathan Marz <nathan@twitter.com>
2012-05-25 12:55:41 -0700

    use storm kryo

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :description "Write Clojure data to and from bytes using Kryo."
-  :dependencies  [[com.twitter/kryo "2.04"]
+  :dependencies  [[storm/kryo "2.04"]
                   [org.clojure/clojure "1.4.0"]]
   :warn-on-reflection true
   :aot :all)
