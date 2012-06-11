7a0b68b0413b0294a6ca65c1228ab7cc58c24ee6
Nathan Marz <nathan@twitter.com>
2012-06-11 15:40:53 -0700

    update to kryo 2.12

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject storm/carbonite "1.2.1"
+(defproject storm/carbonite "1.3.0"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :description "Write Clojure data to and from bytes using Kryo."
-  :dependencies  [[storm/kryo "2.04"]
+  :dependencies  [[com.esotericsoftware.kryo/kryo "2.12"]
                   [org.clojure/clojure "1.4.0"]]
   :warn-on-reflection true
   :aot :all)
